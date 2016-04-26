8448dd8a1cd4e26ce4fbb22119a3d0917d272635
sunderrd <sunderrd@miamioh.edu>
2016-04-26 15:35:21 -0400

    Headers added

---
+++ Libraries and Headers/Glove_Controller_Headers/LilyPad.h
@@ -0,0 +1,29 @@
+//
+//  LilyPad.h
+//  
+//
+//  Created by Ryan Sunderhaus on 4/22/16.
+//
+//
+
+#ifndef LilyPad_h
+#define LilyPad_h
+
+//--Libraries
+
+
+//--Pins
+#define THUMB_PIN   A0
+#define INDEX_PIN   A1
+#define MIDDLE_PIN  A2
+#define CLK         11
+#define DATA        12
+
+//--Flex sensor thresholds
+const int THUMB_THRESHOLD = 55;
+const int INDEX_THRESHOLD = 133;
+const int MIDDLE_THRESHOLD = 150;
+
+
+
+#endif /* LilyPad_h */
