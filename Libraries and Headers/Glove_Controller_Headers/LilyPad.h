d4b89e78e44e962a7da6e77f55afb41c7dd2739d
sunderrd <sunderrd@miamioh.edu>
2016-05-06 14:05:34 -0400

    Final header

--- Libraries and Headers/Glove_Controller_Headers/LilyPad.h
+++ Libraries and Headers/Glove_Controller_Headers/LilyPad.h
@@ -13,17 +13,22 @@
 
 
 //--Pins
-#define THUMB_PIN   A0
-#define INDEX_PIN   A1
-#define MIDDLE_PIN  A2
-#define CLK         11
-#define DATA        12
+#define THUMB_PIN   A3
+#define INDEX_PIN   A2
+#define MIDDLE_PIN  A1
+#define VIBE_PIN    9
+#define LIGHT_PIN   12
+#define RX      0
+#define TX      1
 
 //--Flex sensor thresholds
-const int THUMB_THRESHOLD = 55;
-const int INDEX_THRESHOLD = 133;
-const int MIDDLE_THRESHOLD = 150;
+const int THUMB_THRESHOLD = 235;
+const int INDEX_THRESHOLD = 425;
+const int MIDDLE_THRESHOLD = 375;
 
+//--Vibration constants
+const int VIBE_MAX = 1000;
+const int VIBE_LENGTH = 150;
 
 
 #endif /* LilyPad_h */
